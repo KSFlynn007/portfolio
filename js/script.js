9b381bf7b6b34f1ee44321f30602d817d922b939
KSFlynn007 <kelseyflynnn@gmail.com>
2021-08-31 16:32:00 -0600

    Update script.js
    toggle button for likely large img later

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ $(window).on('load', function() {
     $('.loader').fadeOut(1000);
     $('.content').fadeIn(1000);
 })
+
+let roadmap = $('.roadmap-content');
+let roadmapButton = $('.toggle-roadmap');
+
+roadmapButton.click(function() {
+    roadmap.toggle();
+})
