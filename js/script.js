bef6fa1055b583a80e7aa5a5a8cc686c4581d686
KSFlynn007 <kelseyflynnn@gmail.com>
2022-02-24 12:54:35 -0700

    removed all roadmap ref

--- js/script.js
+++ js/script.js
@@ -2,23 +2,3 @@ $(window).on('load', function() {
     $('.loader').fadeOut(1000);
     $('.content').fadeIn(1000);
 })
-
-let roadmap = $('.roadmap-content');
-let roadmapButton = $('.toggle-roadmap');
-
-roadmap.hide();
-
-roadmapButton.click(function() {
-    roadmap.toggle();
-
-    nameToggle();
-})
-
-
-function nameToggle(){
-    if(roadmapButton.html() == "Open"){
-        roadmapButton.html("Close");
-    } else {
-        roadmapButton.html("Open");
-    }
-}
