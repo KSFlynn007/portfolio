824f9bb48002f7e201801b525ff118d1b5598b7d
KSFlynn007 <kelseyflynnn@gmail.com>
2022-02-24 21:11:51 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ $(window).on('load', function() {
     $('.loader').fadeOut(1000);
     $('.content').fadeIn(1000);
 })
+
+$("#personal").click(function () {
+    $(".personalWork").show(300);
+    $(".studentWork").hide(300);
+})
+
+$("#student").click(function () {
+    $(".personalWork").hide(300);
+    $(".studentWork").show(300);
+})
